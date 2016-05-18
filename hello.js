b8afe651ce2861a46ae3b32934b0c6293114e403
xiongsheng <huoyanwuzhe629@163.com>
2016-05-18 19:06:42 +0800

    test js
    test js

---
+++ hello.js
@@ -0,0 +1,10 @@
+/*
+* @Author: xiongsheng
+* @Date:   2016-05-18 19:04:37
+* @Last Modified by:   xiongsheng
+* @Last Modified time: 2016-05-18 19:05:25
+*/
+
+(function () {
+    alert('hello');
+})();
