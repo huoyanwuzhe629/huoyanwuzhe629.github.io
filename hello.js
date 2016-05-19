d7beffa4eab79010a7d4be2353683ce4f45e0b7f
xiongsheng <huoyanwuzhe629@163.com>
2016-05-19 10:13:20 +0800

    submit
    submit

--- hello.js
+++ hello.js
@@ -2,9 +2,9 @@
 * @Author: xiongsheng
 * @Date:   2016-05-18 19:04:37
 * @Last Modified by:   xiongsheng
-* @Last Modified time: 2016-05-19 10:11:09
+* @Last Modified time: 2016-05-19 10:13:08
 */
 
-window.onload = function () {
-    alert('么么哒');
-};
+(function () {
+    alert('hello');
+})();
