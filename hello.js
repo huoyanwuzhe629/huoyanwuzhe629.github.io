def7a60dd3fddb4d5c6abbee6dd932487f372fc7
xiongsheng <huoyanwuzhe629@163.com>
2016-05-19 10:05:51 +0800

    document ready
    document ready

--- hello.js
+++ hello.js
@@ -2,9 +2,9 @@
 * @Author: xiongsheng
 * @Date:   2016-05-18 19:04:37
 * @Last Modified by:   xiongsheng
-* @Last Modified time: 2016-05-18 19:07:41
+* @Last Modified time: 2016-05-19 10:05:27
 */
 
-(function () {
+document.ready(function () {
     alert('么么哒');
-})();
+});
